850680bde6e8fe4a81bd555dd7f19549af3b2d1e
AbhiCodes <chhettriabhishek9@gmail.com>
2024-07-06 12:46:53 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
 import './App.css'
-import ShoppingBasketRoundedIcon from '@mui/icons-material/ShoppingBasketRounded';
-import LocalGroceryStoreRoundedIcon from '@mui/icons-material/LocalGroceryStoreRounded';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import Header from './Header'
 
 function App() {
   return (
     <div className='App'>
-<div className="header_logo">
-<LocalGroceryStoreRoundedIcon fontSize='large' className='headerLogo_image'/>
-<h2 className="headertitle">eShop</h2>
-</div>
-<div className="header__search">
-<input type="text" className='header_searchInput'/>
-<SearchRoundedIcon className='search_icon'/>
-
-</div>
-<div className="header_nav">
-  <div className="nav_item">
-<span className="item_lineOne">Hello Guest</span>
-<span className="item_lineTwo">Sign IN</span>
-  </div>
-
-  <div className="nav_item">
-  <span className="item_lineOne">Your</span>
-<span className="item_lineTwo">Shop</span>
-  </div>
-
-  <div className="nav_item">
-  <ShoppingBasketRoundedIcon fontSize='large'/>
-<span className="item_lineTwo">0</span>
-  </div>
-    </div>
+<Header/>
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Header.css'
+import ShoppingBasketRoundedIcon from '@mui/icons-material/ShoppingBasketRounded';
+import LocalGroceryStoreRoundedIcon from '@mui/icons-material/LocalGroceryStoreRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header__logo">
+<LocalGroceryStoreRoundedIcon fontSize='large' className='header__logoImage'/>
+<h2 className="header__logoTitle">eShop</h2>
+</div>
+<div className="header__search">
+<input type="text" className='header__searchInput'/>
+<SearchRoundedIcon className='header__searchIcon'/>
+
+</div>
+<div className="header__nav">
+  <div className="nav__item">
+<span className="nav__itemlineOne">Hello Guest</span>
+<span className="nav__itemlineTwo">Sign IN</span>
+  </div>
+
+  <div className="nav__item">
+  <span className="nav__itemLineOne">Your</span>
+<span className="nav__itemlineTwo">Shop</span>
+  </div>
+
+  <div className="nav__item">
+  <ShoppingBasketRoundedIcon fontSize='large' className='nav__itemBasket'/>
+<span className="nav__basketCount">0</span>
+  </div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
