a61b1f52354a255b10117f25fc6df2effe766efc
AbhiCodes <chhettriabhishek9@gmail.com>
2024-07-08 19:27:45 -0400

    updated ratings and added rows to home component

--- src/Prod.jsx
+++ src/Prod.jsx
@@ -6,13 +6,13 @@ function Prod() {
       <div className="product__info">
         <p>
           Queen Bed Frame with Headboard,Linen Upholstered Bed Frame with Wood
-          Slats Support,No Box Spring Needed,Heavy Duty Feet,Easy Assembly
+          Slats Support,No Box Spring Needed
         </p>
         <p className="product__price">
           <small>$</small>
           <strong>30</strong>
         </p>
-        <div className="product__rating">⭐️⭐️⭐️</div>
+        <div className="product__rating">⭐️⭐️⭐️⭐️</div>
       </div>
       <img
         src="https://m.media-amazon.com/images/I/51Ub7jVwu7L._MCnd_AC_.jpg"
